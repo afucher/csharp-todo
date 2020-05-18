d658feec3580cbbef2e51878a71f882c9e0739f2
Arthur Fücher <arthurfucher@gmail.com>
2020-05-17 21:48:05 -0300

    cria servico para tarefas

---
+++ ToDo/Services/ServicoTarefa.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ToDo.Models;
+
+namespace ToDo.Services
+{
+    public class ServiçoTarefa
+    {
+        public IReadOnlyCollection<Tarefa> ObterTarefas()
+        {
+            return new List<Tarefa>();
+        }
+    }
+}
---
+++ ToDoUnitTest/Services/ServicoTarefaTeste.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using ToDo.Services;
+
+namespace ToDoUnitTest.Services
+{
+    public class ServiçoTarefaTeste
+    {
+        [Test]
+        public void DeveTrazerNenhumaTarefa()
+        {
+            //Arrange
+            var serviço = new ServiçoTarefa();
+
+            //Act
+            var tarefas = serviço.ObterTarefas();
+            
+            //Assert
+            Assert.IsEmpty(tarefas);
+        }
+    }
+}
