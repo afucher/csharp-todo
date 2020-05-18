c50d3b6596557c43d1dd626f43b65f03cdd0a254
Arthur Fücher <arthurfucher@gmail.com>
2020-05-17 21:28:02 -0300

    cria excecao especifica para titulo invalido

---
+++ ToDo/Exceptions/TituloInvalidoExcecao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDo.Exceptions
+{
+    public class TítuloInválidoExceção : Exception
+    {
+        public TítuloInválidoExceção() : base("Título deve ter valor.")
+        {
+            
+        }
+    }
+}
---
+++ ToDoUnitTest/Exceptions/TituloInvalidoExcecaoTeste.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using ToDo.Exceptions;
+
+namespace ToDoUnitTest.Exceptions
+{
+    public class TituloInvalidoExcecaoTeste
+    {
+        [Test]
+        public void DeveConterMensagemPadrão()
+        {
+            var exceção = new TítuloInválidoExceção();
+            
+            Assert.AreEqual( "Título deve ter valor.",exceção.Message);
+        }
+    }
+}
