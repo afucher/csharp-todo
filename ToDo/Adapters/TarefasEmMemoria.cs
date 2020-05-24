952a298208d5885ab8f533ad4126db0a95a8a246
Arthur Fücher <arthurfucher@gmail.com>
2020-05-23 22:46:57 -0300

    adiciona fonte de dados em memória

---
+++ ToDo/Adapters/TarefasEmMemoria.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ToDo.Models;
+using ToDo.Services;
+
+namespace ToDo.Adapters
+{
+    public class TarefasEmMemória : IFonteDadosTarefas
+    {
+        List<Tarefa> _tarefas = new List<Tarefa>();
+        public IReadOnlyCollection<Tarefa> ObterTarefas()
+        {
+            return _tarefas;
+        }
+
+        public Tarefa CriarTarefa(Tarefa tarefaParaCriar)
+        {
+            var tarefa = new Tarefa((uint)_tarefas.Count+1, tarefaParaCriar.Título);
+            _tarefas.Add(tarefa);
+            return tarefa;
+        }
+    }
+}
---
+++ ToDoUnitTest/Adapters/TarefasEmMemoriaTeste.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ToDo.Adapters;
+using ToDo.Models;
+
+namespace ToDoUnitTest.Adapters
+{
+    public class TarefasEmMemóriaTeste
+    {
+        [Test]
+        public void DeveRetornarNenhumaTarefa_QuandoInicializado()
+        {
+            var tarefasEmMemória = new TarefasEmMemória();
+
+            var tarefas = tarefasEmMemória.ObterTarefas();
+
+            tarefas.Should().BeEmpty();
+        }
+
+        [Test]
+        public void DeveCriarTarefaERetornarComIdSendoUmNaPrimeiraTarefa()
+        {
+            var tarefasEmMemória = new TarefasEmMemória();
+
+            var tarefa = tarefasEmMemória.CriarTarefa(new Tarefa("título somente"));
+            
+            tarefa.Should().BeEquivalentTo(new Tarefa(1, "título somente"));
+        }
+
+        [Test]
+        public void ObterTarefasDeveRetornarTarefasCriadas()
+        {
+            var tarefasEmMemória = new TarefasEmMemória();
+            tarefasEmMemória.CriarTarefa(new Tarefa("título somente"));
+            tarefasEmMemória.CriarTarefa(new Tarefa("título aleatório mas fixo"));
+
+            var tarefas = tarefasEmMemória.ObterTarefas();
+
+            tarefas.Should().BeEquivalentTo(
+                new {Id = 1, Título = "título somente"}, 
+                                new {Id = 2, Título = "título aleatório mas fixo"});
+        }
+
+        [Test]
+        public void TarefasDevemSerCriadasComIdSequencial()
+        {
+            var tarefasEmMemória = new TarefasEmMemória();
+            var tarefa1 = tarefasEmMemória.CriarTarefa(new Tarefa("tarefa 1"));
+            var tarefa2 = tarefasEmMemória.CriarTarefa(new Tarefa("tarefa 2"));
+            var tarefa3 = tarefasEmMemória.CriarTarefa(new Tarefa("tarefa 3"));
+
+            tarefa1.Id.Should().Be(1);
+            tarefa2.Id.Should().Be(2);
+            tarefa3.Id.Should().Be(3);
+        }
+    }
+}
